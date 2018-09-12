026aebd4e6559356150ac477d4e3e17e91d49766
Yury Korolev <yury.korolev@gmail.com>
2018-09-12 14:08:10 +0300

    udptunnel command

--- Sessions/SSHAgent.m
+++ Sessions/SSHAgent.m
@@ -32,6 +32,37 @@
 
 #import "SSHAgent.h"
 
+static uint32_t _agent_get_u32(const void *vp) {
+  const uint8_t *p = (const uint8_t *)vp;
+  uint32_t v;
+  
+  v  = (uint32_t)p[0] << 24;
+  v |= (uint32_t)p[1] << 16;
+  v |= (uint32_t)p[2] << 8;
+  v |= (uint32_t)p[3];
+  
+  return v;
+}
+
+static void _agent_put_u32(void *vp, uint32_t v) {
+  uint8_t *p = (uint8_t *)vp;
+  
+  p[0] = (uint8_t)(v >> 24) & 0xff;
+  p[1] = (uint8_t)(v >> 16) & 0xff;
+  p[2] = (uint8_t)(v >> 8) & 0xff;
+  p[3] = (uint8_t)v & 0xff;
+}
+
+
+
 @implementation SSHAgent
+
+- (int)processMessage:(NSString *)msg {
+  if (msg.length < 5) {
+    return 0; // Incomplete message header
+  }
+  
+  return 0;
+}
 
 @end
